3d29ca5abd6a77e83f35551903a301d96cac66ec
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-18 15:32:18 +0200

    [add] routers/movieRouter.js (imported express, router, controller, defined index and show routes)

---
+++ routers/movieRouter.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const bookController = require("../controllers/movieController");
+
+// Index
+router.get('/', bookController.index);
+
+// Show
+router.get('/:id', bookController.show);
