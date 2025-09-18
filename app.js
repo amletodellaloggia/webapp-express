4b5bb5130b182737b8a6968c05f0a9e98cee923d
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-18 15:19:54 +0200

    [upd] .gitignore (added .env), [upd] app.js (redefined port and added connection) package.json (added env to watch and start), db.js (added references to env file config)

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const connection = require('./data/db');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+app.use(express.static('public'));
 app.get("/", (req, res) => {
 	res.send("Rotta base della Cineteca")
 });
--- data/db.js
+++ data/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 const connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "root",
-	database: "db_movies",
+	host: process.env.DB_HOST || "localhost",
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE
 });
+
 connection.connect((err) =>{
 	if(err){
 		console.log(`Errore nella connessione al DataBase: ${err}`)
@@ -13,3 +14,5 @@ connection.connect((err) =>{
 		console.log("Connessione al DataBase avvenuta con successo.")
 	};
 });
+
+module.exports = connection;
