d04f129b8f0e0d2afd09f7ca37828196b015d503
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-21 17:14:09 +0200

    [upd] installed cors, imported and used cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const connection = require("./data/db");
 const imagepathMiddleware = require("./middlewares/imagePathMiddleware.js")
 const errorsHandler = require('./middlewares/errorsHandler.js')
@@ -6,6 +7,7 @@ const app = express();
 const port = process.env.PORT;
 const movieRouter = require("./routers/movieRouter");
 
+app.use(cors({origin: process.env.FE_APP}));
 app.use(express.static("public"));
 app.use(imagepathMiddleware);
 app.get("/", (req, res) => {
